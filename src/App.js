0ec4b29a1be9f132377645c2326c5851a6dba846
Anthony Parrino <anthony21parrino21@gmail.com>
2021-06-09 12:19:46 -0400

    added photo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeComp from "../src/component/HomeComp/HomeComp"
+
+function App() {
+  return (
+    <div className="App">
+      <HomeComp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/HomeComp/HomeComp.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./homeComp.css";
+import Photo from "./1.png";
+
+
+function HomeComp() {
+    return (
+        <div>
+            <div className="gerard">
+                <img className="gerardPic" src={Photo} alt="gerard"></img>
+            </div>
+            <div className="inputDiv">
+                <form>
+                    <input id="email" className="inputs" name="email" type="email"></input>
+                    <button id="sendEmailBtn" className="contactBtn" type="submit">s u b m i t</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default HomeComp
