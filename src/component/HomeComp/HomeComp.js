aab336fa7652c22ca1781d1715ace45058d10796
Anthony Parrino <anthony21parrino21@gmail.com>
2021-06-09 14:06:17 -0400

    updates

--- src/component/HomeComp/HomeComp.js
+++ src/component/HomeComp/HomeComp.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import "./homeComp.css";
 import Photo from "./1.png";
+import emailjs from "emailjs-com"
 
 
 function HomeComp() {
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('service_sdicncv', 'template_xdbvob1', e.target, 'user_DhUaWJfNkc42ZUfzTBjVL')
+            .then((result) => {
+                console.log(result.text);
+                alert("Email sent!")
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset()
+        setTimeout(() => {
+            window.location.reload()
+        }, 2000);
+    }
+
     return (
         <div className="gerardMain">
             <div className="gerard">
                 <img className="gerardPic" src={Photo} alt="gerard"></img>
             </div>
             <div className="inputDiv">
-                <form>
+                <form onSubmit={sendEmail}>
                     <div className="btnBorder">
                         <input id="email" className="inputs" name="email" type="email"></input>
                     </div>
-                        <button id="sendEmailBtn" className="contactBtn" type="submit">s u b m i t</button>
+                    <button id="sendEmailBtn" className="contactBtn" type="submit">s u b m i t</button>
                 </form>
             </div>
         </div>
